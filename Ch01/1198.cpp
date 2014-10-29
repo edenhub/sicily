9a03d4ec63bcd4fdea170f8a887075f0c8b1fdb2
edenhub <adamchen.pub@gmail.com>
2014-10-29 11:48:20 +0800

    1198

--- Ch01/1198.cpp
+++ Ch01/1198.cpp
@@ -1,37 +1,67 @@
 #include <iostream>
+
 #define N 8
 
 using namespace std;
+/*
+判断条件为compare的内容
+*/
+bool compare(string &a,string &b){
+	return a+b <= b+a;
+}
+
+void exchange(string &a,string &b){
+	string temp = a;
+	a = b;
+	b = temp;
+}
+
+int partition(string subStrings[],int p,int r){
+	string pivot = subStrings[r];
+	int i = p-1;
+	
+	//i纪录了当前小于pivot的位置，且之前的数都小于pivot
+	//当出现小于pivot的数时，将i之前的数交换跟当前的数交换便可以保持i的作用
+	//最后将pivot的放到i前面，使得pivot起到隔离的作用
+	//放回pivot的位置
+
+	for(int j=p;j<r;j++){
+		if(compare(subStrings[j],pivot)){
+			i++;
+			exchange(subStrings[i],subStrings[j]);
+		}
+	}
+
+	exchange(subStrings[i+1],subStrings[r]);
+
+	return i+1;
+}
+
+void quicksort(string subStrings[],int p,int r){
+	if(p<r){
+		int q = partition(subStrings,p,r);
+		quicksort(subStrings,p,q-1);
+		quicksort(subStrings,q+1,r);
+	}
+}
 
 int main(){
-	int times;
-	cin>>times;
-	string results[N];
+	int testCases,subStringsNumber;
+	string subStrings[N],temp;
 
-	for(int i=0;i<times;i++){
-		int lineNumbers;
-		cin>>lineNumbers;
-		string lines[N],temp;
+	cin>>testCases;
 
-		for(int j=0;j<lineNumbers;j++){
-			cin>>lines[j];
-			// positions[j] = 0;
-		}
+	for(int i=0;i<testCases;i++){
+		cin>>subStringsNumber;
 
-		for(int j=0;j<lineNumbers;j++){
-			temp = lines[j];
-			for(int k=j-1;k>=0;k--){
-				if(temp.compare(lines[k])<0){
-					lines[k+1] = lines[k];
-				}else{
-					lines[k+1] = temp;
-					break;
-				}
-			}
-		}
+		for(int j=0;j<subStringsNumber;j++)
+			cin>>subStrings[j];
 
-		for(int j=0;j<lineNumbers;j++)
-			cout<<lines[j];
+		quicksort(subStrings,0,subStringsNumber-1);
+
+		for(int j=0;j<subStringsNumber;j++)
+			cout<<subStrings[j];
+		cout<<endl;
 	}
 
 	return 0;
