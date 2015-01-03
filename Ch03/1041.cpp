598f1a61c09375562c79f49a82ce3208b5073641
edenhub <adamchen.pub@gmail.com>
2015-01-04 01:08:20 +0800

    1041

--- Ch03/1041.cpp
+++ Ch03/1041.cpp
@@ -30,6 +30,37 @@ void dumpBox(int n,int index){
 	cout<<"."<<endl;
 }
 
+int maxRowStep(int width,int height,int n){
+	int max = 0;
+
+	for(int i=0;i<width;i++){
+		int num = 0;
+		for(int j = 0;j<n;j++){
+			if(box[j].x == i)
+				num++;
+		}
+
+		if(num > max) max = num;
+	}
+	return height - max;
+
+}
+
+int maxColStep(int width,int height,int n){
+	int max = 0;
+	for(int i=0;i<height;i++){
+		int num=0;
+		for(int j=0;j<n;j++){
+			if(box[j].y == i)
+				num++;
+		}
+
+		if(num > max) max = num;
+	}
+
+	return width - max;
+}
+
 bool cmpRight(position a,position b){
 	if(a.x != b.x)
 		return a.x < b.x;
@@ -58,7 +89,11 @@ bool cmpDown(position a,position b){
 		return a.x < b.x;
 }
 
-void moveRight(int off,int n){
+void moveRight(int off,int n,int width,int height){
+	int maxColOff = maxColStep(width,height,n);
+	off = min(maxColOff,off);
+	// int maxRowOff = maxRowStep(width,height,n);
+	// off = min(maxRowOff,off);
 	sort(box,box+n,cmpRight);
 	int x=-1,y;
 	for(int i=0;i<n;i++){
@@ -75,13 +110,17 @@ void moveRight(int off,int n){
 	}
 }
 
-void moveLeft(int off,int n,int width){
+void moveLeft(int off,int n,int width,int height){
+	int maxColOff = maxColStep(width,height,n);
+	off = min(maxColOff,off);
+	// int maxRowOff = maxRowStep(width,height,n);
+	// off = min(maxRowOff,off);
 	sort(box,box+n,cmpLeft);
 	int x=-1,y;
 	for(int i=0;i<n;i++){
 		if(box[i].x != x){
 			x = box[i].x;
-			y = width - off;
+			y = width - off -1;
 		}else{
 			y--;
 		}
@@ -92,7 +131,11 @@ void moveLeft(int off,int n,int width){
 	}
 }
 
-void moveDown(int off,int n){
+void moveDown(int off,int n,int width,int height){
+	int maxRowOff = maxRowStep(width,height,n);
+	off = min(maxRowOff,off);
+	// int maxColOff = maxColStep(width,height,n);
+	// off = min(maxColOff,off);
 	sort(box,box+n,cmpDown);
 	int y=-1,x;
 	for(int i=0;i<n;i++){
@@ -109,13 +152,17 @@ void moveDown(int off,int n){
 	}
 }
 
-void moveUp(int off,int n,int height){
+void moveUp(int off,int n,int width,int height){
+	int maxRowOff = maxRowStep(width,height,n);
+	off = min(maxRowOff,off);
+	// int maxColOff = maxColStep(width,height,n);
+	// off = min(maxColOff,off);
 	sort(box,box+n,cmpUp);
 	int y=-1,x;
 	for(int i=0;i<n;i++){
 		if(box[i].y != y){
 			y = box[i].y;
-			x = height - off;
+			x = height - off -1;
 		}else{
 			x--;
 		}
@@ -133,18 +180,25 @@ void init(int width,int height,int index){
 	for(int i=0;i<n;i++){
 		cin>>box[i].x>>box[i].y;
 	}
+	// int maxRowOff = maxRowStep(width,height,n);
+	// int maxColOff = maxColStep(width,height,n);
 	string op;
 	int off;
 	while((cin>>op) && (op!="done")){
 		cin>>off;
-		if(op == "right")
-			moveRight(off,n);
-		else if(op == "left")
-			moveLeft(off,n,width);
-		else if(op == "down")
-			moveDown(off,n);
-		else if(op == "up")
-			moveUp(off,n,height);
+		if(op == "right"){
+			// off = min(maxColOff,off);
+			moveRight(off,n,width,height);
+		}else if(op == "left"){
+			// off = min(maxColOff,off);
+			moveLeft(off,n,width,height);
+		}else if(op == "down"){
+			// off = min(maxRowOff,off);
+			moveDown(off,n,width,height);
+		}else if(op == "up"){
+			// off = min(maxRowOff,off);
+			moveUp(off,n,width,height);
+		}
 	}
 	dumpBox(n,index);
 }
